e7f9a6a77fb8ea0ec873dbf3f30b839175ab4f74
rafal zygadlo <rafal@maxkod.pl>
2023-11-08 12:51:26 +0100

    New Resource Import

---
+++ app/Filament/Resources/ImportResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ImportResource\Pages;
+use App\Filament\Resources\ImportResource\RelationManagers;
+use App\Models\Import;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ImportResource extends Resource
+{
+    protected static ?string $model = Import::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListImports::route('/'),
+            'create' => Pages\CreateImport::route('/create'),
+            'edit' => Pages\EditImport::route('/{record}/edit'),
+        ];
+    }    
+}
